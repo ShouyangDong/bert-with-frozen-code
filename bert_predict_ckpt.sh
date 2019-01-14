97a492ccf3a9a6fe8f6730ebb197031d3352fb45
SYangDong <shouang.dong@gmail.com>
2019-01-14 13:45:35 +0800

    add shell file to simplify the process

---
+++ bert_predict_ckpt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BERT_BASE_DIR=/path/to/bert/uncased_L-12_H-768_A-12
+export GLUE_DIR=/path/to/glue
+export TRAINED_CLASSIFIER=/path/to/fine/tuned/classifier
+
+python run_classifier.py \
+  --task_name=MRPC \
+  --do_predict=true \
+  --data_dir=$GLUE_DIR/MRPC \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$TRAINED_CLASSIFIER \
+  --max_seq_length=128 \
+  --output_dir=/tmp/mrpc_output/
